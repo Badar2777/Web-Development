aad25b6e37d81197fce7bc200f2c14a1dac71150
BADAR UL ZAMAN <badarulzaman22@gmail.com>
2025-08-28 13:50:30 +0500

    Update tut40.js

--- tut40.js
+++ tut40.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 
-let text = fs.readFileSync('ie.txt')
+let text = fs.readFileSync('ie.txt', 'UTF-8')
 console.log(text)
 
 let file = fs.writeFileSync("2nd.txt", 'Hello this is another file')
+
 console.log(file)
