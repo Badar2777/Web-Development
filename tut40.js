0c9aadacdc38e798eef336d65f9454f78dc4e84e
BADAR UL ZAMAN <badarulzaman22@gmail.com>
2025-09-02 21:37:20 +0500

    tutorial 40

--- tut40.js
+++ tut40.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
 
-let text = fs.readFileSync('ie.txt', 'UTF-8')
+let text = fs.readFileSync("ie.txt" , 'UTF-8')
 console.log(text)
 
 let file = fs.writeFileSync("2nd.txt", 'Hello this is another file')
-
 console.log(file)
